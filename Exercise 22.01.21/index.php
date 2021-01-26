7ec0038be417b3c965c322e0e031f57aeba57c47
LeonardoM <leonardomonticelli19@gmail.com>
2021-01-26 14:22:49 +0100

    26.01

---
+++ Exercise 22.01.21/index.php
@@ -0,0 +1,24 @@
+<?php
+    session_start();
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Exercise</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href=''>
+    <script src=''></script>
+</head>
+<body>
+    <?php
+    {$number=[];
+    array_sum()}
+    ?>
+    <form>
+        <input type="text">
+        <input type="submit" value="SUM">
+    </form>
+</body>
+</html>
---
+++ Exercise 26.01.21/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset='utf-8'>
+    <meta http-equiv='X-UA-Compatible' content='IE=edge'>
+    <title>Tasks revision</title>
+    <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href=''>
+    <script src=''></script>
+</head>
+<body>
+    <?php
+        $items = ["Fruits","Vegetables","Meat","Carrots","Peas","Beans","Tomatoes"];
+        $indices =[1,-1,0,3,4,5,6,2,7,0];
+        $newIndices=[];
+        for($a=0;$a<sizeof($indices);$a++){
+            if($indices[$a]>=0 && $indices[$a]<sizeof($items)){
+                array_push($newIndices,$indices[$a]);
+            }
+        }
+
+        ?>
+        <select><?php
+        for($b=0;$b<sizeof($newIndices);$b++){
+            ?>
+            <option value="<?=$indices[$b]?>">
+            <?php
+        }?>
+        </select>
+            <?php
+    ?>
+</body>
+</html>

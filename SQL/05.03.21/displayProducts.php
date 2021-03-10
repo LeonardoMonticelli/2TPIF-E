9ccc9f0a0a26b78c23a4a657a8666dd8c03c4f15
LeonardoM <leonardomonticelli19@gmail.com>
2021-03-10 14:35:17 +0100

    upd

--- SQL/05.03.21/displayProducts.php
+++ SQL/05.03.21/displayProducts.php
@@ -9,12 +9,17 @@
 <body>
 <?php
    include_once("DBconnect.php");
-
-   $stmt = $connection->prepare("SELECT * from Products");
-   if (!$stmt)
-   {
-      die ("Error in your sql statement...");
-   }
+    if(isset($_POST["filter"])){
+        $stmt = $connection->prepare("SELECT * from Products where Price<=?");
+        $stmt->bind_param("i", $_POST["filter"]);
+    }
+   else{
+        $stmt = $connection->prepare("SELECT * from Products");
+        if (!$stmt)
+        {
+            die ("Error in your sql statement...");
+        }
+    }
    //$stmt->bind_param("si", $_GET["wanted"], $_GET["maxAge"]);
     $stmt->execute();
     $myResult = $stmt->get_result();
@@ -40,7 +45,7 @@
    <?php
     while($row = mysqli_fetch_assoc($myResult)) 
     {
-        if(isset($_POST["filter"]) && $row['Price'] > $_POST["filter"] && $_POST["filter"] != "") continue;
+        // if(isset($_POST["filter"]) && $row['Price'] > $_POST["filter"] && $_POST["filter"] != "") continue;
 
             ?>
             <tr><td><?=$row["ProductName"];?></td><td><?=$row["Price"];?></td></tr>   
